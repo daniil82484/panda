1d9ff676518379df6dbd9c431b391788b2281a87
daniil <daniil82484@gmail.com>
2023-06-06 20:31:36 +0500

    mini-fix

--- app/src/main/java/com/example/panda/ProfileActivity.kt
+++ app/src/main/java/com/example/panda/ProfileActivity.kt
@@ -1,19 +1,25 @@
 package com.example.panda
 
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class ProfileActivity : AppCompatActivity() {
+    lateinit var text: TextView
+    var preff: SharedPreferences?= null
     lateinit var tb: Toolbar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+
+        preff = getSharedPreferences("TABLEE", MODE_PRIVATE)
 
         tb = findViewById(R.id.toolbar)
         setSupportActionBar(this.tb)
@@ -25,6 +31,9 @@ class ProfileActivity : AppCompatActivity() {
 
         val poster_res2: RecyclerView = findViewById(R.id.rec_view2)
         poster_res2.adapter = Achievements_adapter(this, AchievementstList().list)
+
+        text = findViewById(R.id.textView11)
+        text.text = preff?.getString("name", "")
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
